4a1f2e8e692c0d608c88ad3054353b1804f54635
DaSergic <sergiyyershov@yahoo.co.uk>
2016-09-27 09:57:42 +1300

    Update Actionwords.java

--- src/test/java/com/example/Actionwords.java
+++ src/test/java/com/example/Actionwords.java
@@ -1,5 +1,11 @@
 package com.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 public class Actionwords {
 
     public void iOpenP1(String p1) {
